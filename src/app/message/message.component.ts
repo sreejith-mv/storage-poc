467b02a0fe5ef7c6e2d32d9386750931aeec74e8
Sreejith Viswambharan <sreejith.v@pfizer.com>
2022-05-10 17:30:41 +0530

    updated with delete and unred function

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { NavController } from '@ionic/angular';
 import { Message } from '../models/message.model';
+import { MessageService } from '../services/message.service';
 
 @Component({
   selector: 'app-message',
@@ -9,9 +11,24 @@ import { Message } from '../models/message.model';
 export class MessageComponent implements OnInit {
   @Input() message: Message;
 
-  constructor() { }
+  constructor(private navCtrl: NavController, private messageSrv: MessageService) { }
 
-  ngOnInit() {}
+  ngOnInit() { }
+
+  async gotoDetailPage(message: Message) {
+    message.read = true;
+    await this.messageSrv.updateMessage(message);
+    this.navCtrl.navigateForward(`/message/${message.id}`);
+  }
+
+  async delete(message: Message) {
+    await this.messageSrv.removeMessage(message);
+  }
+
+  async unread(message: Message) {
+    message.read = false;
+    await this.messageSrv.updateMessage(message);
+  }
 
   isIos() {
     const win = window as any;
